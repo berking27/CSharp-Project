67e3f6b92f279b0de093209e25fe3ab544cc6575
berkinkoca <berkin.koca01@gmail.com>
2022-07-22 15:33:32 +0300

    Before Config folder

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Entities.Abstract;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //Generic Constraint
+    //Class == refrence type
+    //IEntity => it could be IEntity or a object that implement IEntity
+    //new() => new'lenebilir
+
+    public interface IEntityRepository<T> where T:class,IEntity, new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter = null);
 
--- DataAccess/Concrete/Entity Framework/Context.cs
+++ DataAccess/Concrete/Entity Framework/Context.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using Entities.Abstract;
+using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
     public class Context : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=master;User=sa;Password=reallyStrongPwd123;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
     }
+
 }
 
